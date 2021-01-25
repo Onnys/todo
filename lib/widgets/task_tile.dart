2c32101d3fa1215878d73b5ec9bf39e977ca7d1d
onnys <onnysanild@gmail.com>
2021-01-25 03:13:40 +0200

    Removed some unused imports

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo/models/task_data.dart';
 
 class TaskTile extends StatelessWidget {
   final String title;
